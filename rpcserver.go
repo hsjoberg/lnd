f50e32cd274901ec221aed74c815250cf241e481
djkazic <kevincai96@gmail.com>
2023-04-04 14:40:22 -0400

    Impl addinvoice minroutinghints

--- rpcserver.go
+++ rpcserver.go
@@ -5791,6 +5791,7 @@ func (r *rpcServer) AddInvoice(ctx context.Context,
 		Private:         invoice.Private,
 		RouteHints:      routeHints,
 		Amp:             invoice.IsAmp,
+		MinHopHints:     invoice.MinHopHints,
 	}
 
 	if invoice.RPreimage != nil {
